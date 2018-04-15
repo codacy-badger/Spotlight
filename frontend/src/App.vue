5ae095e57c4987d5951d6e4dfa14171d1e70592b
deen13 <dennis.dierkes@icloud.com>
2018-04-15 21:08:47 +0200

    fixed spotify oauth call

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,9 +5,14 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App',
+    beforeCreate: function () {
+      this.spotify =  JSON.parse('{"' + this.$route.hash.replace(/#/g, '').replace(/&/g, '","').replace(/=/g, '":"') + '"}', function (key, value) {
+        return key === "" ? value : decodeURIComponent(value)
+      });
+    }
+  }
 </script>
 
 <style>
